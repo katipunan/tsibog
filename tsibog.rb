53ffad9b906300afa5b81e1cc6606529413d86ec
Ritchie Paul Buitre <ritchie@richorelse.com>
2014-11-09 23:47:59 +0800

    commented constants

--- tsibog.rb
+++ tsibog.rb
@@ -3,9 +3,10 @@ require 'foursquare2'
 module Tsibog
   CLIENT = Foursquare2::Client.new(:client_id => 'LQSHZK4YX3P5DTIUPRET2EDJ53SZPQSSCXG5IZXDMMQHRR0L', :client_secret => '11FO21GAOBGCIS3S5JEJADXMRH2IIVTIW4DIZANVR4LAV0JE', api_version: 20140806)
   FOOD = '4d4b7105d754a06374d81259'  
-  WALKING_DISTANCE = 80 #meters
-  TEN_MINUTES_AWAY = 800 #meters
-  MAX_RADIUS = 100_000 #meters away
+
+  MAX_RADIUS = 100_000 # meters away
+  WALKING_DISTANCE = 80 # meters or 1 minute walk
+  TEN_MINUTES_AWAY = WALKING_DISTANCE * 10 # minutes
 
   def self.food_places_near(geodata, top = 20)
     CLIENT.search_venues(:ll => geodata, :limit => top, 'categoryId' => FOOD, :radius => TEN_MINUTES_AWAY).venues
