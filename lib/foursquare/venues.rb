4658b69b41a131d933ea99c85bf0f3339d8ed836
Ritchie Paul Buitre <ritchie@richorelse.com>
2014-11-16 15:35:48 +0800

    added #with_category

--- lib/foursquare/venues.rb
+++ lib/foursquare/venues.rb
@@ -1,13 +1,17 @@
 module Foursquare
   class Venues
-  	attr_reader :client, :options
+    attr_reader :client, :options
 
-  	def initialize(client, options = {})
-  	  @client, @options = client, options
-  	end
+    def initialize(client, options = {})
+      @client, @options = client, options
+    end
 
-  	def categories
-  	  (@options['categoryId'] || '').split(',')
-  	end
+    def categories
+      (@options['categoryId'] || '').split(',')
+    end
+
+    def with_category(category)
+      Venues.new @client, 'categoryId' => (categories << category).join(',')
+    end
   end
 end
--- spec/venues_spec.rb
+++ spec/venues_spec.rb
@@ -16,4 +16,26 @@ describe Foursquare::Venues do
   it "has categories" do
     expect(subject.categories).to eq([])
   end
+
+  context "method chaining venues" do
+    let(:food_category) { '4d4b7105d754a06374d81259' }
+
+    describe "#with_category" do
+      before do
+      	@venues = subject.with_category(food_category)
+      end
+
+      it "set with #options['categoryId'] to food_category" do
+        expect(@venues.options['categoryId']).to eq(food_category)
+      end
+
+      it "#categories include food_category" do
+        expect(@venues.categories.include? food_category).to eq(true)
+      end
+    end
+
+    after do
+      expect(@venues.kind_of? Foursquare::Venues).to eq(true)
+    end
+  end
 end
