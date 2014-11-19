09faa9b7d991d04a86debbab4425f11b2f3d2d6d
Ritchie Paul Buitre <ritchie@richorelse.com>
2014-11-19 20:51:59 +0800

    expanded report

--- spec/foursquare3/venues_spec.rb
+++ spec/foursquare3/venues_spec.rb
@@ -8,78 +8,70 @@ describe Foursquare3::Venues do
   let(:food_category) { '4d4b7105d754a06374d81259' }
   let(:latlng) { '14.6371574,121.073077' }
 
-  it "has options" do
-    expect(subject.options).to eq({})
+  context :options do
+    it { expect(subject.options).to eq({}) }
   end
 
-  it "has categories" do
-    expect(subject.categories).to eq([])
+  context :categories do
+    it { expect(subject.categories).to eq([]) }
+  end
+  
+  describe "#with_category" do
+    let(:food_venues) { subject.with_category(food_category) }
+
+    context :categories do
+      it { expect(food_venues.categories).to include(food_category) }
+    end
+
+    context :options do
+      it { expect(food_venues.options).to eq('categoryId' => food_category) }
+    end
   end
 
-  context "set options" do
-    describe "#with_category" do
-      let(:food_venues) { subject.with_category('food_category_id') }
+  describe "#near" do
+    context :options do
+      it { expect(subject.near(latlng).options).to eq(ll: latlng) }
 
-      it { expect(food_venues.categories).to include('food_category_id') }
-
-      context :options do
-        it { expect(food_venues.options).to eq('categoryId' => 'food_category_id') }
+      describe "with accuracy in meters" do
+        it { expect(subject.near(latlng, 10).options).to eq(:ll => latlng, 'llAcc' => 10) }
       end
     end
+  end
 
-    describe "#near" do
-      it "sets geolocation" do
-        expect(subject.near(latlng).options).to eq(ll: latlng)
-      end
+  describe "#above" do
+    context :options do
+      it { expect(subject.above(100).options).to eq(alt: 100) }
 
-      it "sets geolocation with accuracy" do
-        expect(subject.near(latlng, 10).options).to eq(:ll => latlng, 'llAcc' => 10)
-      end
-    end
-
-    describe "#above" do
-      it "sets altitude" do
-        expect(subject.above(100).options).to eq(alt: 100)
-      end
-
-      it "sets altitude with accuracy" do
-        expect(subject.above(100, 1).options).to eq(alt: 100, 'altAcc' => 1)
+      describe "with accuracy in meters" do
+        it { expect(subject.above(100, 1).options).to eq(alt: 100, 'altAcc' => 1) }
       end
     end
+  end
 
-    describe "#within" do
-      it "sets radius" do
-        expect(subject.within(80).options).to eq(radius: 80)
-      end
+  describe "#within" do
+    it { expect(subject.within(80).options).to eq(radius: 80) }
+  end
+
+  describe "#top" do
+    it { expect(subject.top(20).options).to eq(limit: 20) }
+  end
+
+  describe "#search" do
+    it { expect(subject.search('coffee').options).to eq(query: 'coffee') }
+  end
+
+  describe "#for" do
+    it { expect(subject.for('checkin').options).to eq(intent: 'checkin') }
+  end
+
+  context "chain setter methods" do
+    let(:chained) { subject.with_category(food_category).near(latlng).above(100).top(20).search('coffee').for('specials') }
+
+    it "retains options" do
+      expect(chained.options).to eq('categoryId' => food_category, :ll => latlng, :alt => 100, :limit => 20, :query => 'coffee', :intent => 'specials')
     end
 
-    describe "#top" do
-      it "sets limit" do
-        expect(subject.top(20).options).to eq(limit: 20)
-      end
-    end
-
-    describe "#search" do
-      it "sets query" do
-        expect(subject.search('coffee').options).to eq(query: 'coffee')
-      end
-    end
-
-    describe "#for" do
-      it "sets intent" do
-        expect(subject.for('checkin').options).to eq(intent: 'checkin')
-      end
-    end
-
-    context "chain setter methods" do
-      let(:chained) { subject.with_category(food_category).near(latlng).above(100).top(20).search('coffee').for('specials') }
-
-      it "retains options" do
-        expect(chained.options).to eq('categoryId' => food_category, :ll => latlng, :alt => 100, :limit => 20, :query => 'coffee', :intent => 'specials')
-      end
-
-      it { expect(chained).to be_instance_of(subject.class) }
-    end
+    it { expect(chained).to be_instance_of(subject.class) }
   end
 
   describe "when venues are enumerated" do
