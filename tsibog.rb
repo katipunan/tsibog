9f07e02e08d18be2e15e722609b9e51b83d62f38
Ritchie Paul Buitre <ritchie@richorelse.com>
2014-11-20 22:56:35 +0800

    added Tsibog::food_venues removed Tsibog::food_places_near, Tsibog::Application#top & #select_random

--- tsibog.rb
+++ tsibog.rb
@@ -1,5 +1,7 @@
 require 'foursquare2'
 
+require_relative 'lib/tsibog/venues'
+
 module Tsibog
   CLIENT = Foursquare2::Client.new(:client_id => 'LQSHZK4YX3P5DTIUPRET2EDJ53SZPQSSCXG5IZXDMMQHRR0L', :client_secret => '11FO21GAOBGCIS3S5JEJADXMRH2IIVTIW4DIZANVR4LAV0JE', api_version: 20140806)
   FOOD = '4d4b7105d754a06374d81259'  
@@ -8,8 +10,8 @@ module Tsibog
   WALKING_DISTANCE = 80 # meters or 1 minute walk
   TEN_MINUTES_AWAY = WALKING_DISTANCE * 10 # minutes
 
-  def self.food_places_near(geodata, top = 20)
-    CLIENT.search_venues(:ll => geodata, :limit => top, 'categoryId' => FOOD, :radius => TEN_MINUTES_AWAY).venues
+  def self.food_venues
+    Venues.new(CLIENT.method :search_venues).with_category(FOOD).within(TEN_MINUTES_AWAY)
   end
 
   def self.food_place id
@@ -66,14 +68,6 @@ end
 class Tsibog::Application
   COORDINATES_OF = { '47East.ph' => '14.6371574,121.073077' }
 
-  def top(limit = 20)
-    Tsibog.food_places_near(COORDINATES_OF['47East.ph'], limit)
-  end
-
-  def random_select
-    top(20).sample
-  end
-
   def print_details restaurant
     puts restaurant.name
     puts restaurant.address
@@ -82,8 +76,8 @@ class Tsibog::Application
     end
   end
 
-  def initialize id = random_select.id
-    restaurant = Tsibog.food_place id
+  def initialize coordinates = COORDINATES_OF['47East.ph']
+    restaurant = Tsibog.food_place Tsibog.food_venues.near(coordinates).top(20).sample.id
     print_details restaurant
   end
 end
